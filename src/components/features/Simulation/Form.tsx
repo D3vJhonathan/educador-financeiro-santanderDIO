c1f103479c2f7fdae9c87a2a10e94fc5123b55c7
D3vJhonathan <jhonathan.soares65@gmail.com>
2026-08-22 12:32:53 -0300

    🎨 Add basic form setup interface

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,22 +1,17 @@
 import { PiggyBank } from 'lucide-react'
 
+import { simulationFormSteps } from '@/data/simulation'
+
 import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
+  const currentStep = simulationFormSteps[0]
+
   return (
     <>
       <StepProgress currentStep={1} totalSteps={3} />
-      <FormStep
-        icon={PiggyBank}
-        title="Renda Mensal Bruta"
-        question="Quanto é depositado na sua conta todo mês (soma de todas as fontes)?"
-        inputProps={{
-          type: 'text',
-          placeholder: 'Ex: 2.500,00',
-          prefix: 'R$',
-        }}
-      />
+      <FormStep key={currentStep.id} {...currentStep} />
     </>
   )
 }
