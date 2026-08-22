75bfc38c247c38a6db9590b3362936a8daf65a27
D3vJhonathan <jhonathan.soares65@gmail.com>
2026-08-22 13:21:52 -0300

    🎨 Add navigation between forms

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,4 +1,5 @@
 import { PiggyBank } from 'lucide-react'
+import { useState } from 'react'
 
 import { simulationFormSteps } from '@/data/simulation'
 
@@ -6,12 +7,38 @@ import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
-  const currentStep = simulationFormSteps[0]
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev - 1)
+  }
 
   return (
     <>
-      <StepProgress currentStep={1} totalSteps={3} />
-      <FormStep key={currentStep.id} {...currentStep} />
+      <StepProgress
+        currentStep={currentStepIndex + 1}
+        totalSteps={totalSteps}
+      />
+      <FormStep
+        key={currentStep.id}
+        {...currentStep}
+        onBack={handlePreviousStep}
+        onNext={handleNextStep}
+        hideBackButton={currentStepIndex === 0}
+      />
     </>
   )
 }
